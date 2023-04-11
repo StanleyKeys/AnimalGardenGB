7a71807b191aee4bb0207cbbcd1285bb34f74ac9
StanleyGunz <stanislavkinsler@gmail.com>
2023-04-11 21:18:53 +0700

    v 0.1 Created app, classes for animals

---
+++ src/main/java/GardenApp.java
@@ -0,0 +1,43 @@
+
+
+public class GardenApp {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Animal {
+
+}
+
+class Pet extends Animal {
+
+}
+
+class Pack extends Animal {
+
+}
+
+class Dog extends Pet {
+
+}
+
+class Cat extends Pet {
+
+}
+
+class Hamster extends Pet {
+
+}
+
+class Horse extends Pack {
+
+}
+
+class Camel extends Pack {
+
+}
+
+class Donkey extends Pack {
+
+}
